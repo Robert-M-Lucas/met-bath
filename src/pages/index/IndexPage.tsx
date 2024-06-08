f3b51968a07e34edf41c86f199d3caef2a8bfa63
Robert-M-Lucas <robert-m-lucas@example.com>
2024-06-08 11:19:51 +0100

    Update

--- src/pages/index/IndexPage.tsx
+++ src/pages/index/IndexPage.tsx
@@ -1,10 +1,17 @@
-
+import {useNavigate} from "react-router-dom";
 
 
 function IndexPage() {
+    const navigate = useNavigate();
+    const onclick = () => {
+        const id = Math.floor(Math.random() * 1_000_000_000) + 1;
+        navigate("/user/" + id);
+    };
+
     return (<>
         <h1>Index</h1>
-        </>);
+        <button type="button" className="btn btn-outline-primary" onClick={onclick}>Random User</button>
+    </>);
 }
 
 export default IndexPage;
